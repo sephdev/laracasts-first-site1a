afb3a4e8020eda0ce973bccd2fb58d319f106fb1
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-04-17 15:00:35 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('title')
+    About Us
+@endsection
+
+@section('content')
+    <h1>About Us</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae aliquam placeat modi dicta laborum quas. Nisi magnam corrupti officia quam deserunt ea deleniti? Numquam totam aut debitis corporis, natus inventore?</p>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+    Home - My Laracasts
+@endsection
+
+@section('content')
+    <h1>Welcome</h1>
+@endsection
